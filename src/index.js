a2768247a89b453c605d3b8f0077ba3519340503
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-10 17:00:13 +0700

    Setting up React

--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
-import { createStore } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { createPost, editPost } from './actions';
-import appReducer from './reducers';
-
-const store = createStore(appReducer);
-
-store.subscribe(() => {
-  console.log('state changed:', store.getState());
-});
-
-const root = document.getElementById('root');
-
-const render = () => {
-  root.innerHTML = '';
-  const { posts } = store.getState();
-  posts.forEach((post, index) => {
-    const item = document.createElement('li');
-    item.addEventListener('click', () => store.dispatch(editPost(index, `${post.text}!`)));
-    const text = document.createTextNode(
-      `${post.user} - ${post.text}`,
-    );
-    item.appendChild(text);
-    root.appendChild(item);
-  });
-};
-store.subscribe(render);
-
-store.dispatch(createPost('dan', 'hello world'));
-store.dispatch(createPost('des', 'second post'));
+ReactDOM.render(
+  <h1>hello world!</h1>,
+  document.getElementById('root'),
+);
