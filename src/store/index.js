00e783170f1ce0405d1f173f48ca333e14bf8bcf
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-11 18:48:00 +0700

    Using Redux DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
-import { createStore } from 'redux';
-
+import { createStore, compose } from 'redux';
 import appReducer from '../reducers';
 
+const enhancer = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
 export default function configureStore(initialState) {
-  const store = createStore(appReducer, initialState);
+  const store = createStore(
+    appReducer,
+    initialState,
+    enhancer,
+  );
 
   return store;
 }
